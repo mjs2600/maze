8e21c1b8a43bb4fbb3206487ca29de0d21762baa
Devin Walters <devinw@gmail.com>
2013-07-10 13:54:23 -0500

    Add jsonex dependency

--- mix.exs
+++ mix.exs
@@ -8,14 +8,11 @@ defmodule Maze.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [{:jsonex,"2.0",[github: "marcelog/jsonex", tag: "2.0"]}]
   end
 end
